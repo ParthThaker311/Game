7ded71501403a90d030f84d9aaa5164dc6d6eeeb
k0ofdeath <parth.thaker.311@gmail.com>
2015-11-17 12:14:52 -0500

    Added player, zombie, and floor along with PlayerMovement script

---
+++ ZombMe/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private float speed=0.1f;
+	private Rigidbody rb;
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal*speed, 0.0f, moveVertical*speed);
+		rb.position = rb.position + movement;
+	}
+}
