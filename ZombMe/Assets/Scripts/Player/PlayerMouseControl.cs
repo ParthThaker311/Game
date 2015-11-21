7778744f6b588e96498d04666cde049f482fc7a1
k0ofdeath <parth.thaker.311@gmail.com>
2015-11-21 00:31:44 -0500

    attempting to do camera rotation
    (didnt work well)

---
+++ ZombMe/Assets/Scripts/Player/PlayerMouseControl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMouseControl : MonoBehaviour {
+	private Vector2 mousePos;
+	private Vector3 screenPos;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mousePos = Input.mousePosition;
+		screenPos = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, transform.position.z - Camera.main.transform.position.z));
+		GetComponent<Rigidbody>().transform.eulerAngles = new Vector3(0,0,Mathf.Atan2((mousePos.y - transform.position.y), (mousePos.x - transform.position.x))*Mathf.Rad2Deg - 90);
+	}
+}
